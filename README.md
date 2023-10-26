7d9f6f740abf2423874aaeaca7fc9b4327873fc9
maomi777777 <148947521+maomi777777@users.noreply.github.com>
2023-10-26 21:07:36 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# jdseckill
+京东抢购，抢遥遥领先，茅台
+![seckill.png](seckill.png)
---
+++ main.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace jdseckill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("天下没有免费的午餐");
+        }
+    }
+}
